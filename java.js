64c8ee72afdaa3fc1c1cb6fe3142ec981c1c2f56
maram121‏ <maram_mohra@hotmail.com>
2021-04-22 10:52:36 +0300

    first commit

---
+++ java.js
@@ -0,0 +1,10 @@
+let a = prompt("enter a string");
+if (a=="red")
+alert("apple");
+else if (a=="yellow")
+alert("banana")
+ else if(a=="green")
+alert("grass");
+ else if(a=="circle"||a=="rectangle"||a=="square")
+alert("shape");
+else alert("i don't know");
